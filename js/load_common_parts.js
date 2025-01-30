d3c93f18f37709d43563cb75bba4e20bf118a8db
まゆC# | mayu___cs <hato182@gmail.com>
2025-01-31 00:26:56 +0900

    パートナーバナーも共通化
    ついでにヘッダーとフッターをdivじゃなくて対応するタグに変更

--- js/load_common_parts.js
+++ js/load_common_parts.js
@@ -1,9 +1,14 @@
-// ヘッダー読み込み(共通)
+// ヘッダー読み込み
 fetch("common/header.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("#header").innerHTML = data);
 
-// フッター読み込み(共通)
+// フッター読み込み
 fetch("common/footer.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("#footer").innerHTML = data);
+
+// パートナーバナー読み込み
+fetch("common/partner_baner.html")
+    .then((response) => response.text())
+    .then((data) => document.querySelector("#partner_baner").innerHTML = data);
